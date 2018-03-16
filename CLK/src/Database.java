298736c65074207626b2ccc0cc80a5fe336d7848
Naymat <naymat30@gmail.com>
2018-03-16 02:11:57 -0400

    Added methods to add courses to student SQLite table

--- CLK/src/Database.java
+++ CLK/src/Database.java
@@ -16,10 +16,13 @@ public class Database {
 
     private void initializeStudents() throws SQLException {
         Statement statement = connection.createStatement();
+        //Creates a students table with columns StudentId,FirstName,LastName,Courses
+        //If it already exists, it ignores the create table command
         statement.execute("CREATE TABLE IF NOT EXISTS students(" +
                 "StudentId integer PRIMARY KEY," +
                 "FirstName text NOT NULL," +
-                "LastName text NOT NULL" +
+                "LastName text NOT NULL," +
+                "Courses text" +
                 ");");
     }
 
@@ -39,12 +42,13 @@ public class Database {
      * @throws ClassNotFoundException - if unable to locate SQLite library
      */
     public void addStudent(int id, String firstName, String lastName) throws SQLException, ClassNotFoundException {
-        //If there isn't a connection we call getConnection and
+        //If there isn't a connection we call getConnection and create it there
         if(connection == null){
             getConnection();
         }
-
+        //PreparedStatement creates template for SQLite insert
         PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO students(StudentId,FirstName,LastName) values(?,?,?);");
+        //Set what ? means below for the preparedStatement
         preparedStatement.setInt(1, id);
         preparedStatement.setString(2,firstName);
         preparedStatement.setString(3,lastName);
@@ -52,11 +56,54 @@ public class Database {
         preparedStatement.execute();
     }
 
+    /**
+     * Gets a students courses from student SQLite table
+     * Multiple course are delimited by : and stored in the format: coursecode:coursecode:coursecode
+     * Eg: cps590:cps406:cps412
+     * @param studetnId -integer Student id
+     * @throws SQLException
+     */
+    public  String getCourses(int studetnId) throws SQLException {
+        String courses;
+        ResultSet result;
+
+        //Searches Database by StudentId and selects Courses and StudentId
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT StudentId,Courses FROM students WHERE StudentId=?;");
+        preparedStatement.setInt(1,studetnId);
+        //Gets the value in the Courses field for the given StudentId
+        result = preparedStatement.executeQuery();
+        courses = result.getString("Courses");
+
+        return courses;
+    }
+
+    public void addCourse(int studentId, String newCourse) throws SQLException {
+        String courses;
+        //gets courses string from SQLite student table using getCourses()
+        courses = getCourses(studentId);
+        //if there are no courses, we make the courses equal to the new course
+        if(courses == null){
+            courses = newCourse;
+        }
+        else {
+            courses += ":"+ newCourse;
+        }
+
+        PreparedStatement preparedStatement = connection.prepareStatement("UPDATE students SET Courses = ? WHERE StudentId=?;");
+        preparedStatement.setString(1,courses);
+        preparedStatement.setInt(2,studentId);
+
+        preparedStatement.execute();
+
+
+    }
     public ResultSet searchStudents(String sqlStatement) throws SQLException {
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(sqlStatement);
         return resultSet;
     }
+
+
 
 }
 
--- CLK/src/StudentTester.java
+++ CLK/src/StudentTester.java
@@ -1,26 +1,41 @@
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class StudentTester {
     public static void main(String [] args)
     {
         Database clickerDatabase = new Database();
 
+        Scanner scan = new Scanner(System.in);
         String studentId;
+        int intStudentId;
         String firstName;
         String lastName;
-        try {
-            clickerDatabase.getConnection();
-            clickerDatabase.addStudent(50133412,"John","Doe");
-            ResultSet resultSet = clickerDatabase.searchStudents("SELECT StudentId,FirstName,LastName FROM students");
-            while (resultSet.next()){
-                studentId = resultSet.getString("StudentId");
-                firstName = resultSet.getString("FirstName");
-                lastName = resultSet.getString("LastName");
-                System.out.println(studentId + " " + firstName + " " + lastName);
 
-            }
+        try {
+            /*
+            System.out.println("Enter Student id: ");
+            studentId = scan.nextLine();
+            System.out.println("Enter your First Name:");
+            firstName = scan.nextLine();
+            System.out.println("Enter last name");
+            lastName = scan.nextLine();
+            intStudentId = Integer.parseInt(studentId);
+            System.out.println();
+            */
+
+            clickerDatabase.getConnection();
+            //clickerDatabase.addStudent(intStudentId,firstName,lastName);
+            ResultSet resultSet = clickerDatabase.searchStudents("SELECT StudentId,FirstName,LastName FROM students");
+            /*while (resultSet.next()){
+
+                System.out.println(resultSet.getString("StudentId") + " " + resultSet.getString("FirstName")
+                        + " " + resultSet.getString("LastName") + " Courses: ");
+            }*/
+            clickerDatabase.addCourse(50078292, "cps393");
+            System.out.println("50078292 Courses: " + clickerDatabase.getCourses(50078292));
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
